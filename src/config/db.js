7b252239631d6f01b19dc5bd7332ab85779baf54
kumarmentalmodel <kumarmanglamemail@gmail.com>
2024-04-17 20:24:41 +0530

    updated db config

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,5 @@
 const Sequelize = require("sequelize");
+const pg = require("pg");
 const { DataTypes } = Sequelize;
 const path = require("path");
 const res = require("dotenv").config({
@@ -6,6 +7,7 @@ const res = require("dotenv").config({
 });
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
+  dialectModule: pg,
   ssl: {
     rejectUnauthorized: false,
   },
