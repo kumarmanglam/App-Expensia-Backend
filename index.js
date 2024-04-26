96680c555426e4fa338a8ca0141d229f208c4ed1
kumarmentalmodel <kumarmanglamemail@gmail.com>
2024-04-26 20:20:00 +0530

    backend update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 
 // app.use(express.static("public"));
 app.get("/", async (req, res) => {
-  res.send("Expensia backend BASE API");
+  res.send("Expensia Backend API is working");
 });
 app.use(express.json());
 app.use("/auth", userRouter);
